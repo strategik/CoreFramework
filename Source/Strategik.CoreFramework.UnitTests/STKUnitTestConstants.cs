95616b19b8e7ceeecd154b53f3b7f905d312fb10
Dr Adrian Colquhoun <adrian@strategik.com.au>
2015-11-18 09:02:15 +1000

    Adding unit tests assembly and testing continuing build

---
+++ Source/Strategik.CoreFramework.UnitTests/STKUnitTestConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategik.CoreFramework.UnitTests
+{
+    public static class STKUnitTestConstants
+    {
+        public const String CoreFramework_SanityChecks = "CoreFramework.SanityChecks";
+    }
+}
